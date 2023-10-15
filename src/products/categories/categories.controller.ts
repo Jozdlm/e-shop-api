461e32617e88121f17ef1f8e7cfe922b4be3c539
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-15 09:41:44 -0600

    Define controlador categories que obtienes las categorias de la db

--- src/products/categories/categories.controller.ts
+++ src/products/categories/categories.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './category.entity';
+import { Repository } from 'typeorm';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  public constructor(
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>
+  ) {}
+
+  @Get()
+  public findAll(): Promise<Category[]> {
+    return this.categoriesRepository.find();
+  }
+}
