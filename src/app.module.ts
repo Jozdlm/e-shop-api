d63aa32cc2d0084e987ad1483281c37a1770ad99
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-15 09:53:09 -0600

    Define un prefix para las rutas del products-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Category } from './products/categories/category.entity';
 import { ProductsModule } from './products/products.module';
+import { RouterModule, Routes } from '@nestjs/core';
 
 const SOURCE_OPTIONS: TypeOrmModuleOptions = {
   type: 'mysql',
@@ -16,8 +17,19 @@ const SOURCE_OPTIONS: TypeOrmModuleOptions = {
   synchronize: true,
 };
 
+const ROUTES: Routes = [
+  {
+    path: 'api',
+    module: ProductsModule,
+  },
+];
+
 @Module({
-  imports: [TypeOrmModule.forRoot(SOURCE_OPTIONS), ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot(SOURCE_OPTIONS),
+    RouterModule.register(ROUTES),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
