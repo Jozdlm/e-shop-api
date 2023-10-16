1c526cb1acedac66c236a438b4fcbd73815987c9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-15 21:33:53 -0600

    Instala el paquete nestjs-config y actualiza la configuración del source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { Category } from './products/categories/category.entity';
 import { ProductsModule } from './products/products.module';
 import { RouterModule, Routes } from '@nestjs/core';
 import { Product } from './products/product.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-const SOURCE_OPTIONS: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'devroot',
-  database: 'libreria_lajoya',
-  entities: [Category, Product],
-  synchronize: false,
+const DB_ASYNC_OPTIONS: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get('DATABASE_HOST'),
+    port: +configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USERNAME'),
+    password: configService.get('DATABASE_PASSWORD'),
+    database: configService.get('DATABASE_NAME'),
+    entities: [Category, Product],
+    synchronize: false,
+  }),
 };
 
 const ROUTES: Routes = [
@@ -27,7 +32,10 @@ const ROUTES: Routes = [
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(SOURCE_OPTIONS),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(DB_ASYNC_OPTIONS),
     RouterModule.register(ROUTES),
     ProductsModule,
   ],
