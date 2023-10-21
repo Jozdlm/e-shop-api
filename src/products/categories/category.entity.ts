6471bb08078cc58c2d86d859cd8548bf405e3966
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-20 21:38:00 -0600

    Añade una nueva propiedad a la entidad Category

--- src/products/categories/category.entity.ts
+++ src/products/categories/category.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
-  name: 'product_categories'
+  name: 'product_categories',
 })
 export class Category {
   @PrimaryGeneratedColumn()
@@ -9,13 +9,20 @@ export class Category {
 
   @Column({
     type: 'varchar',
-    length: 50
+    length: 50,
+    unique: true,
+  })
+  public slug: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
   })
   public name: string;
 
   @Column({
     type: 'varchar',
-    length: 255
+    length: 255,
   })
   public description: string;
 }
